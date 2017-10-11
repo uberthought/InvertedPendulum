20a5e7e797ec5e6f50c0960b5fa6bdd009b71109
root <uberthought@gmail.com>
2017-10-11 13:46:05 +0000

    fix merge error

--- network.py
+++ network.py
@@ -7,14 +7,11 @@ class DNN:
     def __init__(self, state_size, action_size):
         self.input_layer = tf.placeholder(tf.float32, shape=(None, state_size))
 
-        self.hidden1 = tf.layers.dense(inputs=self.noise, units=state_size * 2, activation=tf.nn.relu)
+        self.hidden1 = tf.layers.dense(inputs=self.input_layer, units=state_size * 2, activation=tf.nn.relu)
         self.hidden2 = tf.layers.dense(inputs=self.hidden1, units=state_size * 2, activation=tf.nn.relu)
         self.hidden3 = tf.layers.dense(inputs=self.hidden2, units=state_size * 2, activation=tf.nn.relu)
-        # self.hidden4 = tf.layers.dense(inputs=self.hidden3, units=state_size * 2, activation=tf.nn.relu)
 
         self.prediction = tf.layers.dense(inputs=self.hidden3, units=action_size)
-
-        self.prediction = tf.layers.dense(inputs=hidden3, units=action_size)
         self.expected = tf.placeholder(tf.float32, shape=(None, action_size))
 
         self.train_loss = tf.reduce_mean(tf.losses.mean_squared_error(self.expected, self.prediction))
